ddd09021ed3c667dbaef882c0b8e0c9987e5e778
tmr232 <tmr232@github>
2013-03-28 00:01:59 +0200

    Added manifest and basic removal script.

---
+++ source/unCover.js
@@ -0,0 +1,5 @@
+div = document.querySelector("div#cover");
+div.parentNode.removeChild(div);
+img = document.querySelector("div#signUpPhoto");
+img.parentNode.removeChild(img);
+console.log("UnCovered!");
