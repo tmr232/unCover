d7145ea54da4b8e6b0479f15c8d4b987596f0b88
tmr232 <tmr232@github>
2013-04-01 19:34:37 +0300

    Replaced JS for CSS and added wolframAlpha support.

--- source/unCover.js
+++
@@ -1,5 +0,0 @@
-div = document.querySelector("div#cover");
-div.parentNode.removeChild(div);
-img = document.querySelector("div#signUpPhoto");
-img.parentNode.removeChild(img);
-console.log("UnCovered!");
